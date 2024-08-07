22678a8377918071a2fa2563109936a85241f7ef
tiendung <tiendung8a6@gmail.com>
2024-08-07 14:18:18 +0700

    install TypeScript

---
+++ typescript/01intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: 'John', age: 10 };
+console.log("Hello");
+console.log("Your name is ", user.name);
---
+++ typescript/01intro/intro.ts
@@ -0,0 +1,4 @@
+let user = {name : 'John', age : 10 }
+
+console.log("Hello");
+console.log("Your name is " , user.name);
