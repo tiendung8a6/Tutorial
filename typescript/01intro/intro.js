150c812f36ecefc7869135555f6f13635db447bf
tiendung <tiendung8a6@gmail.com>
2024-08-15 09:13:29 +0700

    new commit

--- typescript/01intro/intro.js
+++
@@ -1,3 +0,0 @@
-var user = { name: 'John', age: 10 };
-console.log("Hello");
-console.log("Your name is ", user.name);
--- typescript/01intro/intro.ts
+++
@@ -1,4 +0,0 @@
-let user = {name : 'John', age : 10 }
-
-console.log("Hello");
-console.log("Your name is " , user.name);
